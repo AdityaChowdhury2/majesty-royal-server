f0a1cce670203dd099b446bf3bc4f598005f8c38
AdityaChowdhury2 <adityac5102@gmail.com>
2023-11-05 11:20:45 +0600

    user api created to store user data

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middlewares
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}
+));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -35,6 +39,18 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+const usersCollection = client.db("hotelDb").collection("users");
+
+app.post('/api/v1/users', async (req, res) => {
+    try {
+        const newUser = req.body;
+        const result = await usersCollection.insertOne(newUser)
+        res.send(result)
+    } catch (error) {
+        res.send({ message: "Error inserting user" })
+    }
+})
 
 app.get('/', (req, res) => {
     res.send("Welcome to my Hostel Booking application!");
