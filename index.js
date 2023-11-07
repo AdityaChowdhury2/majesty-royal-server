c75ec0f48a869c3c365460fff718341d56500033
Aditya <adityac5102@gmail.com>
2023-11-08 04:36:47 +0600

    fixing cookie related issues

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
 
 //middlewares
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://majesty-royal-aditya.web.app'],
-    credentials: true,
-}
-));
-app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://majesty-royal-aditya.web.app",
+        "https://majesty-royal-aditya.firebaseapp.com"
+    ], credentials: true
+}));
+app.use(express.json());
 
-const verifyUser = (req, res, next) => {
-    // console.log(req.cookies.token);
-    const token = req?.cookies?.token;
-    if (!token) {
-        return res.status(401).send({ message: "Unauthorized" })
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: "unauthorized" })
-        }
-        req.user = decoded
-        next()
-    })
-}
 
 // Mongodb Connection
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.3fz84ur.mongodb.net/?retryWrites=true&w=majority`;
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -41,7 +30,20 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
+const verifyUser = (req, res, next) => {
+    // console.log(req.cookies.token);
+    const { token } = req.cookies;
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized" })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unauthorized" })
+        }
+        req.user = decoded
+        next();
+    })
+}
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -49,6 +51,9 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -60,23 +65,6 @@ const usersCollection = client.db("hotelDb").collection("users");
 const roomsCollection = client.db("hotelDb").collection("rooms");
 const bookingsCollection = client.db("hotelDb").collection("bookings");
 const reviewsCollection = client.db("hotelDb").collection("reviews");
-
-app.post('/api/v1/user', async (req, res) => {
-    try {
-        const user = req.body;
-        const checkIfExists = await usersCollection.findOne({ email: user.email });
-        if (!checkIfExists) {
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        }
-    } catch (error) {
-        res.send({ message: "Error inserting user" })
-    }
-})
-
-
-
-
 
 app.get('/api/v1/room', async (req, res) => {
     try {
@@ -90,11 +78,9 @@ app.get('/api/v1/room', async (req, res) => {
         if (sortingOrder) {
             sortByPrice.price = Number(sortingOrder);
         }
-        const projection = { roomName: 1, _id: 1, price: 1, thumbnailImage: 1, specialOffer: 1, seatsAvailable: 1 };
+        const projection = { roomName: 1, _id: 1, price: 1, thumbnailImage: 1, specialOffer: 1, seatsAvailable: 1, reviewCount: 1 };
         const result = await roomsCollection.find(query).sort(sortByPrice).project(projection).skip(skip).limit(4).toArray();
         const total = await roomsCollection.countDocuments(query);
-        // console.log(req.url);
-        // console.log(total);
         res.send({ result, total });
     } catch (error) {
         res.send({ error: "Couldn't find" })
@@ -112,22 +98,32 @@ app.get("/api/v1/room/:id", verifyUser, async (req, res) => {
     }
 })
 
-app.patch('/api/v1/room/:id', async (req, res) => {
+app.patch('/api/v1/room/:id', verifyUser, async (req, res) => {
     try {
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const seatsBooked = req.body.seatsCount;
-        const operation = {
-            $inc: { "seatsAvailable": -seatsBooked }
+        const filter = { _id: new ObjectId(id) };
+        const seatsBooked = req.body?.seatsCount;
+        const reviewCount = req.body?.reviewCount;
+        const updatedRoom = {}
+        if (seatsBooked) {
+            updatedRoom['$inc'] = { "seatsAvailable": -seatsBooked }
         }
-        const result = await roomsCollection.findOneAndUpdate(query, operation)
+        if (reviewCount) {
+            updatedRoom['$set'] = {
+                "reviewCount": reviewCount
+            }
+
+        }
+        // console.log(updatedRoom);
+        const result = await roomsCollection.findOneAndUpdate(filter, updatedRoom)
         res.send(result);
     } catch (error) {
 
     }
 })
 
-app.get('/api/v1/bookings', async (req, res) => {
+// todo: check if 403
+app.get('/api/v1/bookings', verifyUser, async (req, res) => {
     const query = {}
     const bookingDate = req?.query?.bookingDate;
     const roomId = req?.query?.roomId;
@@ -141,16 +137,39 @@ app.get('/api/v1/bookings', async (req, res) => {
     if (uid) {
         query.uid = uid;
     }
-    console.log(query);
+    // console.log(query);
     const result = await bookingsCollection.find(query).toArray();
-    console.log(result);
     res.send(result);
 })
+
+app.delete('/api/v1/bookings/:bookingId', verifyUser, async (req, res) => {
+    try {
+        const bookingId = req.params.bookingId;
+        const result = await bookingsCollection.deleteOne({ _id: new ObjectId(bookingId) })
+        res.send(result);
+    } catch (error) {
+        res.send({ message: "Internal Server Error", error: error })
+    }
+})
+
+app.post('/api/v1/user', async (req, res) => {
+    try {
+        const user = req.body;
+        const checkIfExists = await usersCollection.findOne({ email: user.email });
+        if (!checkIfExists) {
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        }
+    } catch (error) {
+        res.send({ message: "Error inserting user" })
+    }
+})
+
 
 app.post('/api/v1/user/book-room', verifyUser, async (req, res) => {
     try {
         const bookingDetails = req.body;
-        console.log(bookingDetails);
+        // console.log(bookingDetails);
         const result = await bookingsCollection.insertOne(bookingDetails)
         res.send(result);
     } catch (error) {
@@ -161,9 +180,10 @@ app.post('/api/v1/user/book-room', verifyUser, async (req, res) => {
 app.post('/api/v1/review', verifyUser, async (req, res) => {
     try {
         const review = req.body;
+
         const today = new Date();
         review.timeStamp = today.toISOString();
-        console.log(review);
+        // console.log(review);
         const result = await reviewsCollection.insertOne(review)
         res.send(result);
     } catch (error) {
@@ -174,38 +194,50 @@ app.post('/api/v1/review', verifyUser, async (req, res) => {
 app.get('/api/v1/reviews/', async (req, res) => {
     try {
         const roomId = req?.query?.roomId
+        const limit = Number(req?.query?.limit) || 0;
         const filter = {}
         if (roomId) {
             filter.roomId = roomId;
         }
-        const result = await reviewsCollection.find(filter).toArray()
+        const result = await reviewsCollection.find(filter).limit(limit).toArray();
         res.send(result)
     } catch (error) {
 
     }
 })
 
+app.post('/api/v1/user/create-token', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "1h" })
 
+    res.cookie(
+        "token",
+        token,
+        {
+            maxAge: 3600 * 24,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        }
+    ).send({ message: "Token created successfully" });
 
-app.post('/api/v1/user/create-token', (req, res) => {
-    try {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "12h" })
-        res.cookie('token', token, { httpOnly: false, sameSite: "none", maxAge: 2 * 60 * 60 * 1000, secure: true }).send({ message: "Token created successfully" });
-    } catch (error) {
-        res.send({ message: "Error creating token" })
-    }
 })
 
-app.post('/api/v1/user/logout', (req, res) => {
-    try {
-        const user = req.body;
-        res.clearCookie('token', { httpOnly: false, sameSite: "none", secure: true }).send({ message: "Token expired" });
-    }
-    catch (err) {
-        res.send({ message: "Error logging out" })
-    }
+app.post('/api/v1/user/logout', async (req, res) => {
+
+    const user = req.body;
+    res.clearCookie(
+        "token",
+        {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        }
+    ).send({ message: 'logout' })
+
 })
+
+
 
 
 app.get('/', (req, res) => {
