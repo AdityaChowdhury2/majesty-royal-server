2bc121ebad99634c3e19b296a6941f55db72d435
AdityaChowdhury2 <adityac5102@gmail.com>
2023-12-01 19:06:15 +0600

    refactor backend code

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 5000;
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
 
 //middlewares
+require('dotenv').config()
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        "http://localhost:5173",
-        "https://majesty-royal-aditya.web.app",
-        "https://majesty-royal-aditya.firebaseapp.com"
-    ], credentials: true
+        'https://majesty-royal-aditya.web.app',
+        'http://localhost:5173',
+
+    ],
+    credentials: true
 }));
-app.use(express.json());
 
 
 // Mongodb Connection
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.3fz84ur.mongodb.net/?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -30,9 +30,25 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (err) {
+        console.log(err);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run();
+
+
 const verifyUser = (req, res, next) => {
-    // console.log(req.cookies.token);
-    const token = req.cookies.token;
+    const token = req?.cookies?.token;
     if (!token) {
         return res.status(401).send({ message: "Unauthorized" })
     }
@@ -40,12 +56,12 @@ const verifyUser = (req, res, next) => {
         if (err) {
             return res.status(401).send({ message: "unauthorized" })
         }
-        req.user = decoded
-        next();
+        else {
+            req.user = decoded
+            next();
+        }
     })
-
 }
-
 
 const usersCollection = client.db("hotelDb").collection("users");
 const roomsCollection = client.db("hotelDb").collection("rooms");
@@ -69,6 +85,7 @@ app.get('/api/v1/room', async (req, res) => {
         const total = await roomsCollection.countDocuments(query);
         res.status(200).send({ result, total });
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -80,6 +97,7 @@ app.get("/api/v1/room/:id", verifyUser, async (req, res) => {
         const result = await roomsCollection.findOne(query);
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -104,6 +122,7 @@ app.patch('/api/v1/room/:id', verifyUser, async (req, res) => {
         console.log(result);
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -122,17 +141,17 @@ app.get('/api/v1/bookings', verifyUser, async (req, res) => {
             query.roomId = roomId;
         }
         if (userEmail) {
-            if (userEmail !== decodedEmail) {
-                return res.status(403).send({ message: "Forbidden Access" })
-            }
-            else {
-                query.email = userEmail;
-            }
+            // if (userEmail !== decodedEmail) {
+            //     return res.status(403).send({ message: "Forbidden Access" })
+            // }
+            // else {
+            query.email = userEmail;
+            // }
         }
-        console.log(query);
         const result = await bookingsCollection.find(query).toArray();
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -143,6 +162,7 @@ app.delete('/api/v1/bookings/:bookingId', verifyUser, async (req, res) => {
         const result = await bookingsCollection.deleteOne({ _id: new ObjectId(bookingId) })
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -158,6 +178,7 @@ app.patch('/api/v1/bookings/:bookingId', verifyUser, async (req, res) => {
         const result = await bookingsCollection.findOneAndUpdate(filter, updatedBooking)
         res.send(result)
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -174,6 +195,7 @@ app.post('/api/v1/user', async (req, res) => {
             res.status(200).send({ message: "User already exists" })
         }
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -186,6 +208,7 @@ app.post('/api/v1/user/book-room', verifyUser, async (req, res) => {
         console.log("room booking status: " + result);
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -198,6 +221,7 @@ app.post('/api/v1/review', verifyUser, async (req, res) => {
         const result = await reviewsCollection.insertOne(review)
         res.status(200).send(result);
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
@@ -213,54 +237,56 @@ app.get('/api/v1/reviews/', async (req, res) => {
         const result = await reviewsCollection.find(filter).limit(limit).toArray();
         res.status(200).send(result)
     } catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Internal Server Error" })
     }
 })
 
 app.post('/api/v1/user/create-token', async (req, res) => {
-    const user = req.body;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "1h" })
-    res.cookie(
-        "token",
-        token,
-        {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production" ? true : false,
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        }
-    ).status(200).send({ message: "Token created successfully" });
+    try {
+        const user = req.body;
+        const tokenValue = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "1h" })
+        res.cookie(
+            "token",
+            tokenValue,
+            {
+                httpOnly: false,
+                sameSite: "none",
+                secure: true
+                // httpOnly: false,
+                // secure: process.env.NODE_ENV === "production" ? true : false,
+                // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            }
+        ).status(200).send({ message: "Token created successfully" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Internal Server Error" })
+    }
 })
 
 app.post('/api/v1/user/logout', async (req, res) => {
-    const user = req.body;
-    res.clearCookie(
-        "token",
-        {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === "production" ? true : false,
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        }
-    ).status(200).send({ message: 'logout' })
-
-})
-
-
-async function run() {
     try {
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-    } catch (err) {
-        console.log("Failed to connect");
+        const user = req.body;
+        res.clearCookie(
+            "token",
+            {
+                maxAge: 0,
+                sameSite: "none",
+                secure: true
+                // maxAge: 0,
+                // secure: process.env.NODE_ENV === "production" ? true : false,
+                // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            }
+        ).status(200).send({ message: 'logout' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Internal Server Error" })
     }
-}
-run().catch(console.dir);
+})
 
 app.get('/', (req, res) => {
     res.send("Welcome to my Hostel Booking application!");
 })
-
-
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
